c531526522cc1a2fcfc1f16f574555ea909da811
Bruel <78389090+TristanBruel@users.noreply.github.com>
2022-12-20 09:09:53 +0100

    Add files via upload

--- inputs/PSD/PSDs.py
+++ inputs/PSD/PSDs.py
@@ -1,18 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 # Standard python numerical analysis imports:
 import numpy as np
-
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 
 # Get ET spectrum
 ET_D_psd=np.loadtxt('ET_D_sensitivity.txt', skiprows=0)
 
 # Get CE spectrum
-CE1_psd=np.loadtxt('curves_Jan_2020/ce1.txt', skiprows=0)
+#CE1_psd=np.loadtxt('curves_Jan_2020/ce1.txt', skiprows=0)
+CE1_psd=np.loadtxt('ce_strain/cosmic_explorer_20km.txt', skiprows=0)
 
 # Get CE spectrum
-CE2_psd=np.loadtxt('curves_Jan_2020/ce2.txt', skiprows=0)
+#CE2_psd=np.loadtxt('curves_Jan_2020/ce2.txt', skiprows=0)
+CE2_psd=np.loadtxt('ce_strain/cosmic_explorer.txt', skiprows=0)
 
 # Get aLIGO spectrum
 AL_psd=np.loadtxt('ALIGO_sensitivity.txt', skiprows=7)
@@ -25,32 +28,46 @@ KAG_psd=np.loadtxt('KAGRA_sensitivity.txt', skiprows=7)
 
 
 #########################################################################
+fmin = 5
+fmax = 5000
 
-# plot the spectra
-fmin=10
-fmax=3000
+### Plotting parameters ###
+CB_color_cycle = ['#377eb8', '#ff7f00', '#4daf4a',
+                  '#f781bf', '#a65628', '#984ea3',
+                  '#999999', '#e41a1c', '#dede00']
+CB_marker_cycle = ['solid', 'dashed', 'dotted',
+                   'solid', 'dashed', 'dotted',
+                   'solid', 'dashed', 'dotted']
 
-fig1 = plt.figure()
-colors = sns.color_palette(None, 6)
-plt.loglog(KAG_psd[:,0],KAG_psd[:,5],color=colors[0],label='KAGRA design')
-plt.loglog(ADV_psd[:,0],ADV_psd[:,6],color=colors[1],label='Virgo design')
-plt.loglog(AL_psd[:,0],AL_psd[:,6],color=colors[2],label='aLIGO design')
-plt.loglog(ET_D_psd[:,0],ET_D_psd[:,3],color=colors[3],label='ET')
-plt.loglog(CE1_psd[:,0],CE1_psd[:,1],color=colors[4],label='CE stage 1')
-#plt.loglog(CE1_psd[:,0],4*CE1_psd[:,1],color=colors[5],label='CE 20km')
-plt.loglog(CE2_psd[:,0],CE2_psd[:,1],color=colors[5],label='CE stage 2')
+fs = 20
+lw=3
 
+plt.rc('font', family='serif')
+plt.rc('lines', linewidth=lw)
+custom_cycler = plt.cycler(color=CB_color_cycle)
+plt.rc('axes', prop_cycle=custom_cycler)
 
-#plt.axis([1, fmax, 1e-25, 4e-20])
+fig, ax = plt.subplots(1, 1, figsize=(10, 7))
 
-plt.grid(True)
-plt.ylabel('ASD [strain/sqrt(Hz)]')
-plt.xlabel('Frequency [Hz]')
-plt.legend(loc='upper right')
+plt.loglog(AL_psd[:,0],AL_psd[:,5], label='aLIGO design')
+plt.loglog(ADV_psd[:,0],ADV_psd[:,5], label='Virgo late high')
+plt.loglog(KAG_psd[:,0],KAG_psd[:,5], label='KAGRA design')
+plt.loglog(ET_D_psd[:,0],ET_D_psd[:,3], label='ET')
+plt.loglog(CE1_psd[:,0],CE1_psd[:,1], label='CE 20km')
+plt.loglog(CE2_psd[:,0],CE2_psd[:,1], label='CE 40km') 
 
-fig_name = 'PSDs_2.png';
-plt.savefig(fig_name)
-fig_name = 'PSDs_2.pdf';
-plt.savefig(fig_name)
+# Set labels and legend
+ax.grid(True)
+ax.set_xlabel('Frequency [Hz]', fontsize=fs)
+ax.set_xlim(xmin=fmin, xmax=fmax)
+ax.set_ylabel('ASD [strain/sqrt(Hz)]', fontsize=fs)
+ax.set_ylim(ymin=1e-25, ymax=1e-21)
+ax.legend(loc='upper right', fontsize=0.8*fs)
+ax.tick_params(labelsize=0.8*fs)
+
+fig_name = 'PSDs.png';
+plt.savefig(fig_name, bbox_inches='tight')
+fig_name = 'PSDs.pdf';
+plt.savefig(fig_name, bbox_inches='tight')
     
 plt.show()
